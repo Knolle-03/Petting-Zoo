d6833ad972bed8447ab3cec49d0012f274516f1c
Knolle-03 <Knolle-03@users.noreply.github.com>
2018-12-10 06:24:06 +0100

    removed test lines

--- dog.rb
+++ dog.rb
@@ -17,28 +17,4 @@ class Dog < Pet
     @owner = owner
     @owner.add_pet(self)
   end
-=begin
-  def ==(other)
-    return false if other.nil?
-    return true if self.equal?(other)
-    return false unless self.class == other.class
-    [@name, @birthday, @lives, @owner] == [other.name, other.birthday, other.lives, other.owner]
-  end
-
-  def hash
-    prime = 31
-    result = 0
-    result = prime * result + (@name.nil? ? 0 : @name.hash)
-    result = prime * result + (@birthday.nil? ? 0 : @birthday.hash)
-    result = prime * result + (@lives.nil? ? 0 : @lives.hash)
-    prime * result + (@owner.nil? ? 0 : @owner.hash)
-  end
-
-  def eql?(other)
-    return false if other.nil?
-    return true if self.equal?(other)
-    return false unless self.class == other.class
-    [@name, @birthday, @lives, @owner] == [other.name, other.birthday, other.lives, other.owner]
-  end
-=end
 end
